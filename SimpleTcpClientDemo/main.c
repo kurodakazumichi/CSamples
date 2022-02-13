ecbb40502e49af6fc157a031ef733fbf847c8489
kazumichi kuroda <kazumichi96@gmail.com>
2022-02-14 03:21:32 +0900

    First Commit

---
+++ SimpleTcpClientDemo/main.c
@@ -0,0 +1,64 @@
+﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#include <stdio.h>
+#include <WinSock2.h>
+
+// AF_INET: AFはAddress Familyの略だろう、INETはIPv4で通信するという事を指定する。
+// sin_familyなどのsinはsockaddr_inを略したsin、またsockaddr_inはsocket addreess, internetの略
+int main() {
+	WSADATA wsaData;
+	struct sockaddr_in server;
+	SOCKET sock;
+	char buf[2048];
+
+	// winsock2の初期化
+	int err = WSAStartup(MAKEWORD(2, 0), &wsaData);
+
+	if (err != 0) {
+		switch (err) {
+		case WSASYSNOTREADY: // ネットワークサブシステムがネットワークへの接続を準備できてない
+			printf("WSASYSNOTREADY\n");
+			break;
+		case WSAVERNOTSUPPORTED: // winsockのバージョンがサポートされていない
+			printf("WSAVERNOTSUPPORTED\n");
+			break;
+		case WSAEINPROGRESS:
+			printf("WSAEINPROGRESS\n");
+			break;
+		case WSAEPROCLIM:
+			printf("WSAEPROCLIM\n");
+			break;
+		case WSAEFAULT:
+			printf("WSAEFAULT\n");
+			break;
+		}
+	}
+
+	// ソケットの作成
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (sock == INVALID_SOCKET) {
+		printf("error : %d\n", WSAGetLastError());
+		return 1;
+	}
+
+	// 接続先指定用構造体の準備
+	server.sin_family = AF_INET;
+	server.sin_port = htons(12345);
+	server.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+
+	// サーバーに接続
+	connect(sock, (struct sockaddr*)&server, sizeof(server));
+
+	send(sock, "Hello", 5, 0);
+
+	// サーバーからデータを受信
+	memset(buf, 0, sizeof(buf));
+	int n = recv(sock, buf, sizeof(buf), 0);
+	printf("%d, %s\n", n, buf);
+
+	// winsock2の終了
+	WSACleanup();
+
+	return 0;
+}
---
+++ SimpleTcpServerDemo/main.c
@@ -0,0 +1,113 @@
+﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <WinSock2.h>
+
+// 用語
+// WSA = Windows Sockets API
+int main()
+{
+	// Windows Sockets APIのデータを格納する変数を用意する
+	WSADATA wsaData;
+	SOCKET sock0;
+	struct sockaddr_in addr;
+	struct sockaddr_in client;
+	int len;
+	SOCKET sock;
+	int n;
+	char buf[2048];
+	char inbuf[2048];
+
+	// WSAStartupはソケットプログラムを開始するための準備をするもので、一度だけ呼ぶ必要がある。
+	// 第一引数には16bitの変数にAPIのバージョンを指定して渡す。
+	int err = WSAStartup(MAKEWORD(2, 0), &wsaData);
+
+	if (err != 0) {
+		switch (err) {
+		case WSASYSNOTREADY: // ネットワークサブシステムがネットワークへの接続を準備できてない
+			printf("WSASYSNOTREADY\n");
+			break;
+		case WSAVERNOTSUPPORTED: // winsockのバージョンがサポートされていない
+			printf("WSAVERNOTSUPPORTED\n");
+			break;
+		case WSAEINPROGRESS:
+			printf("WSAEINPROGRESS\n");
+			break;
+		case WSAEPROCLIM:
+			printf("WSAEPROCLIM\n");
+			break;
+		case WSAEFAULT:
+			printf("WSAEFAULT\n");
+			break;
+		}
+		return 1;
+	}
+
+	// ソケットの作成
+	sock0 = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (sock0 == INVALID_SOCKET) {
+		printf("error : %d\n", WSAGetLastError());
+		return 1;
+	}
+
+	// ソケットの設定
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(12345);
+	addr.sin_addr.S_un.S_addr = INADDR_ANY;
+
+	if (bind(sock0, (struct sockaddr*)&addr, sizeof(addr)) != 0) {
+		printf("error : %d\n", WSAGetLastError());
+		return 1;
+	}
+
+	// TCPクライアントからの接続要求を待てる状態にする
+	if (listen(sock0, 5) != 0) {
+		printf("error : %d\n", WSAGetLastError());
+		return 1;
+	}
+
+	// レスポンスメッセージを作成
+	memset(buf, 0, sizeof(buf));
+	_snprintf(
+		buf, sizeof(buf),
+		"HTTP/1.1 200 OK \r\n"
+		"Content-Type: text/html\r\n"
+		"\r\n"
+		"HELLO\r\n"
+	);
+
+	// TCPクライアントからの接続要求を受け付ける
+	while (1) {
+		len = sizeof(client);
+		sock = accept(sock0, (struct sockaddr*)&client, &len);
+
+		if (sock == INVALID_SOCKET) {
+			printf("error : %d\n", WSAGetLastError());
+			break;
+		}
+
+		printf("accepted connection:\n");
+		printf("from=%s\n", inet_ntoa(client.sin_addr));
+		printf("port=%d\n", ntohs(client.sin_port));
+
+		memset(inbuf, 0, sizeof(inbuf));
+		recv(sock, inbuf, sizeof(inbuf), 0);
+		printf("%s", inbuf);
+
+		// データ送信
+		n = send(sock, buf, (int)strlen(buf), 0);
+
+		if (n < 1) {
+			printf("error : %d\n", WSAGetLastError());
+			break;
+		}
+
+		closesocket(sock);
+	}
+
+	WSACleanup();
+
+	return 0;
+}
